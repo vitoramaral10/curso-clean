c31fabe40a2eacd805e443c060fcc5d4c10a3d93
Vitor Amaral de Melo <vitor.amaral10@gmail.com>
2024-01-19 14:13:16 -0300

    Refactor authentication use case to use AuthenticationParams

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -2,8 +2,12 @@ import 'package:curso_clean/domain/entities/entities.dart';
 import 'package:meta/meta.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({
-    @required String email,
-    @required String password,
-  });
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String email;
+  final String secret;
+
+  AuthenticationParams({@required this.email, @required this.secret});
 }
