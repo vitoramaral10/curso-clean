b1e56fadaec0eccc7d4c20fd9524921592cfb7d0
Vitor Amaral de Melo <vitor.amaral10@gmail.com>
2024-01-19 17:09:16 -0300

    Add handling for 400 Bad Request response

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,4 @@
+import 'package:curso_clean/data/http/http.dart';
 import 'package:curso_clean/infra/http/http.dart';
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
@@ -82,6 +83,14 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, null);
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
     });
   });
 }
