4f7c80ae16e42c7db44009884e619a1f1548ee75
Vitor Amaral de Melo <vitor.amaral10@gmail.com>
2024-01-19 11:59:05 -0300

    Refactor RemoteAuthenticationTest to include HTTP method in request

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,7 +1,7 @@
 import 'package:faker/faker.dart';
+import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
@@ -10,24 +10,30 @@ class RemoteAuthentication {
   RemoteAuthentication({@required this.httpClient, @required this.url});
 
   Future<void> auth() async {
-    await httpClient.request(url: url);
+    await httpClient.request(url: url, method: 'post');
   }
 }
 
 abstract class HttpClient {
-  Future<void> request({@required String url}) async {}
+  Future<void> request({
+    @required String url,
+    @required String method,
+  }) async {}
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test('Should call HttClient with correct URL', () async {
+  test('Should call HttClient with correct values', () async {
     final httpClient = HttpClientSpy();
     final url = faker.internet.httpUrl();
     final sut = RemoteAuthentication(httpClient: httpClient, url: url);
 
     await sut.auth();
 
-    verify(httpClient.request(url: url));
+    verify(httpClient.request(
+      url: url,
+      method: 'post',
+    ));
   });
 }
