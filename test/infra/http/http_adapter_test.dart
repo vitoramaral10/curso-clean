83fcd74e0de8044e946ecb261d35454160e8bc66
Vitor Amaral de Melo <vitor.amaral10@gmail.com>
2024-01-19 17:09:45 -0300

    Add test for handling 400 response in HttpAdapter

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -92,5 +92,13 @@ void main() {
 
       expect(future, throwsA(HttpError.badRequest));
     });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
   });
 }
