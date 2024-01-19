3a23f11670cc7e16db619f982a526f800bb97368
Vitor Amaral de Melo <vitor.amaral10@gmail.com>
2024-01-19 16:26:45 -0300

    Refactor HTTPAdapter to handle null body in post request

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -21,7 +21,9 @@ class HttpAdapter {
       'accept': 'application/json',
     };
 
-    await client.post(url, headers: headers, body: jsonEncode(body));
+    final jsonBody = body != null ? jsonEncode(body) : null;
+
+    await client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -53,6 +55,14 @@ void main() {
               'accept': 'application/json',
             },
             body: '{"any_key":"any_value"}'),
+      );
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(
+        client.post(any, headers: anyNamed('headers')),
       );
     });
   });
