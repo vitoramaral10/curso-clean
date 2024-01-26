c16d1da0aa41d2262a1e6562e4b87ac6437234e8
Vitor Amaral de Melo <vitor.amaral10@gmail.com>
2024-01-26 13:10:31 -0300

    Refactor login button onPressed callback

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -65,7 +65,11 @@ class LoginPage extends StatelessWidget {
                         stream: presenter!.isFormValidStream,
                         builder: (context, snapshot) {
                           return ElevatedButton(
-                            onPressed: snapshot.data == true ? () {} : null,
+                            onPressed: snapshot.data == true
+                                ? () {
+                                    presenter?.auth();
+                                  }
+                                : null,
                             child: Text('Entrar'.toUpperCase()),
                           );
                         }),
