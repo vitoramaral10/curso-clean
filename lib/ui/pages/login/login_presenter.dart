0cb6ed59019e79acfa497a28e45afb9e06e810d7
Vitor Amaral de Melo <vitor.amaral10@gmail.com>
2024-01-26 12:46:15 -0300

    Refactor email validation in LoginPage

--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -1,5 +1,6 @@
 abstract class LoginPresenter {
-  void validateEmail(String email);
+  Stream<String> get emailErrorStream;
 
+  void validateEmail(String email);
   void validatePassword(String password);
 }
--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:curso_clean/ui/pages/pages.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
@@ -8,15 +10,24 @@ class LoginPresenterSpy extends Mock implements LoginPresenter {}
 
 void main() {
   late LoginPresenter presenter;
+  late StreamController<String> emailErrorController;
 
   setUp(() {
     presenter = LoginPresenterSpy();
+    emailErrorController = StreamController<String>();
   });
 
   Future<void> loadPage(WidgetTester tester) async {
+    when(() => presenter.emailErrorStream)
+        .thenAnswer((_) => emailErrorController.stream);
+
     final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage);
   }
+
+  tearDown(() {
+    emailErrorController.close();
+  });
 
   testWidgets('Should load with correct inicial state',
       (WidgetTester tester) async {
@@ -49,5 +60,15 @@ void main() {
     final password = faker.internet.password();
     await tester.enterText(find.bySemanticsLabel('Senha'), password);
     verify(() => presenter.validatePassword(password));
+  });
+
+  testWidgets('Should present error if email is invalid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    emailErrorController.add('any error');
+    await tester.pump();
+
+    expect(find.text('any error'), findsOneWidget);
   });
 }
