1cc759e5d973a69127b2e95e38103f90c14e11f9
Vitor Melo <vitor.amaral10@gmail.com>
2024-01-26 22:03:53 -0300

    Update user authentication logic

---
+++ test/validation/validators/required_field_validation_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Should return null if value is not empty', () {
+    final sut = RequiredFieldValidation('any_field');
+
+    final error = sut.validate('any_value');
+
+    expect(error, null);
+  });
+
+  test('Should return error if value is empty', () {
+    final sut = RequiredFieldValidation('any_field');
+
+    final error = sut.validate('');
+
+    expect(error, 'Campo obrigatório');
+  });
+}
+
+abstract class FieldValidation {
+  String get field;
+  String? validate(String value);
+}
+
+class RequiredFieldValidation implements FieldValidation {
+  @override
+  final String field;
+
+  RequiredFieldValidation(this.field);
+
+  @override
+  String? validate(String value) {
+    return value.isEmpty ? 'Campo obrigatório' : null;
+  }
+}
