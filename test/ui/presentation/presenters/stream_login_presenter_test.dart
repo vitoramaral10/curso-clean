e58e32d6be88729e8c5c6f1c63168c97a9274194
Vitor Melo <vitor.amaral10@gmail.com>
2024-01-26 21:16:38 -0300

    Refactor validation in StreamLoginPresenterTest

--- test/ui/presentation/presenters/stream_login_presenter_test.dart
+++ test/ui/presentation/presenters/stream_login_presenter_test.dart
@@ -37,10 +37,19 @@ void main() {
   late ValidationSpy validation;
   late String email;
 
+  When mockValidationCall({String? field}) => when(() => validation.validate(
+      field: field ?? any(named: 'field'), value: any(named: 'value')));
+
+  void mockValidation({String? field, String? value}) {
+    mockValidationCall(field: field).thenReturn(value);
+  }
+
   setUp(() {
     validation = ValidationSpy();
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
+
+    mockValidation();
   });
 
   test('Should call Validation with correct email', () async {
@@ -50,9 +59,7 @@ void main() {
   });
 
   test('Should emit email error if validation fails', () {
-    when(() => validation.validate(
-        field: any(named: 'field'),
-        value: any(named: 'value'))).thenReturn('error');
+    mockValidation(value: 'error');
 
     expectLater(sut.emailErrorStream, emits('error'));
 
