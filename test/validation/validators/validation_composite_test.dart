3037a2c4398032ef30d936e0b6149f11eefbf690
Vitor Melo <vitor.amaral10@gmail.com>
2024-01-26 22:50:08 -0300

    Refactor validation composite test

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -4,15 +4,39 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 void main() {
-  test('Should return null if all validations returns null or empty', () {
-    final validation1 = FieldValidationSpy();
-    final validation2 = FieldValidationSpy();
-    when(() => validation1.field).thenReturn('other_field');
-    when(() => validation1.validate(any())).thenReturn('error_1');
-    when(() => validation2.field).thenReturn('any_field');
-    when(() => validation2.validate(any())).thenReturn(null);
+  late ValidationComposite sut;
+  late FieldValidation validation1;
+  late FieldValidation validation2;
+  late FieldValidation validation3;
 
-    final sut = ValidationComposite([validation1, validation2]);
+  void mockValidation1(String? error) {
+    when(() => validation1.validate(any())).thenReturn(error);
+  }
+
+  void mockValidation2(String? error) {
+    when(() => validation2.validate(any())).thenReturn(error);
+  }
+
+  void mockValidation3(String? error) {
+    when(() => validation3.validate(any())).thenReturn(error);
+  }
+
+  setUp(() {
+    validation1 = FieldValidationSpy();
+    when(() => validation1.field).thenReturn('any_field');
+    mockValidation1(null);
+    validation2 = FieldValidationSpy();
+    when(() => validation2.field).thenReturn('any_field');
+    mockValidation2(null);
+    validation3 = FieldValidationSpy();
+    when(() => validation3.field).thenReturn('other_field');
+    mockValidation3(null);
+
+    sut = ValidationComposite([validation1, validation2]);
+  });
+
+  test('Should return null if all validations returns null or empty', () {
+    mockValidation2('');
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
     expect(error, null);
