6f45f462fa67c47b3b067524a11c5f58d4d6095c
Vitor Melo <vitor.amaral10@gmail.com>
2024-01-26 21:12:27 -0300

    Fix email error notification in login presenter

--- test/ui/presentation/presenters/stream_login_presenter_test.dart
+++ test/ui/presentation/presenters/stream_login_presenter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -6,13 +8,25 @@ abstract class Validation {
   String? validate({required String field, required String value});
 }
 
+class LoginState {
+  late String? emailError;
+}
+
 class StreamLoginPresenter {
   final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+
+  final _state = LoginState();
+
+  Stream<String?> get emailErrorStream => _controller.stream.map(
+        (state) => state.emailError,
+      );
 
   StreamLoginPresenter({required this.validation});
 
   void validateEmail(String email) {
-    validation.validate(field: 'email', value: email);
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
   }
 }
 
@@ -33,5 +47,15 @@ void main() {
     sut.validateEmail(email);
 
     verify(() => validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test('Should emit email error if validation fails', () {
+    when(() => validation.validate(
+        field: any(named: 'field'),
+        value: any(named: 'value'))).thenReturn('error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
   });
 }
