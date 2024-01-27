05a1c089b91b7f6db0f754c547dd048b579a51fc
Vitor Melo <vitor.amaral10@gmail.com>
2024-01-26 22:28:02 -0300

    Add email validation test cases

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,4 +1,5 @@
 import 'package:curso_clean/validations/protocols/field_validation.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -14,6 +15,15 @@ void main() {
   test('Should return null if email is null', () {
     expect(sut.validate(null), null);
   });
+
+  test('Should return null if email is valid', () {
+    final email = faker.internet.email();
+    expect(sut.validate(email), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('any_value'), 'Campo inválido');
+  });
 }
 
 class EmailValidation implements FieldValidation {
@@ -24,6 +34,10 @@ class EmailValidation implements FieldValidation {
 
   @override
   String? validate(String? value) {
-    return null;
+    final regex = RegExp(r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$',
+        caseSensitive: false);
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value!);
+
+    return isValid ? null : 'Campo inválido';
   }
 }
