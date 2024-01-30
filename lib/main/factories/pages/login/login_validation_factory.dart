49514e05fc30fd9e9f3955f4e662fd75b4603747
Vitor Amaral de Melo <vitor.amaral10@gmail.com>
2024-01-30 17:07:11 -0300

    Refactor login validation factory

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,11 +1,13 @@
 import '../../../../presentation/protocols/protocols.dart';
 import '../../../../validations/protocols/protocols.dart';
 import '../../../../validations/validators/validators.dart';
+import '../../../builders/builders.dart';
 
 Validation makeLoginValidation() => ValidationComposite(makeLoginValidations());
 
-List<FieldValidation> makeLoginValidations() => [
-      const RequiredFieldValidation('email'),
-      const EmailValidation('email'),
-      const RequiredFieldValidation('password'),
-    ];
+List<FieldValidation> makeLoginValidations() {
+  return [
+    ...ValidationBuilder.field('email')!.required().email().build(),
+    ...ValidationBuilder.field('password')!.required().build(),
+  ];
+}
